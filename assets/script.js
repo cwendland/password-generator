09e9bc40f8bd8b443528f652beeb7e955f6d4432
cwendland <chris.r.wendland@gmail.com>
2022-08-18 12:46:31 -0700

    Added lowercase as an option and added choice validation.

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var generateBtn = document.querySelector("#generate");
 var boolSpecial = false;
 var boolUpper = false;
 var boolNumbers = false;
+var boolLower = false;
 
 //Strings of the possible password components depending on user choice
 var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
@@ -13,28 +14,25 @@ var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numberChars = "1234567890";
 var lowerChars = upperChars.toLowerCase();
 
-// Write password to the #password input
-function generatePassword(passLength, inclSpecial, inclUpper, inclNumbers) {
-  var availChars = lowerChars;
+function validateChoices() { //Prompts user choice to determine if they would like special characters, uppercase characters, and numbers to be added to the password
+  if (confirm("Would you like to include special characters?")) {
+    boolSpecial = true;
+  }
+  if (confirm("Would you like to include uppercase characters?")) {
+    boolUpper = true;
+  }
+  if (confirm("Would you like to include lowercase characters?")) {
+    boolLower = true;
+  }
+  if (confirm("Would you like to include number characters?")) {
+    boolNumbers = true;
+  }
 
-  //Conditional statements to determine if optional password components need to be added to the password component string
-  if (inclSpecial) {
-    availChars = availChars + specialChars;
+  if( !(boolNumbers || boolSpecial || boolUpper || boolLower) ) {
+    alert("No choices were made. Atleast one character choice must be made");
+    validateChoices();
   }
-  if (inclUpper) {
-    availChars = availChars + upperChars;
-  }
-  if (inclNumbers) {
-    availChars = availChars + numberChars;
-  }
-
-  var password = []; //Creates password array, needs to be array so push works.
-
-  for(var i = 0; i < passLength; i++) { //For loop for the password length
-    password.push(availChars[Math.floor(Math.random()*availChars.length)]) //Randomly choose an element of the availChars string to be added to the password
-  }
-  return password.join(""); //Join the array into a string so the return is a string
-} 
+}
 
 function obtainInput() { //Function to get the password length and make sure input is a number and is between 8 and 128 inclusive
   var numChars = prompt("How many characters would you like the password to be?"); //Get the desired password length from the user
@@ -51,20 +49,39 @@ function obtainInput() { //Function to get the password length and make sure inp
 }
 
 
+// Write password to the #password input
+function generatePassword(passLength, inclSpecial, inclUpper, inclNumbers, inclLower) {
+  var availChars = "";
+
+  //Conditional statements to determine if optional password components need to be added to the password component string
+  if (inclSpecial) {
+    availChars = availChars + specialChars;
+  }
+  if (inclUpper) {
+    availChars = availChars + upperChars;
+  }
+  if (inclNumbers) {
+    availChars = availChars + numberChars;
+  }
+  if (inclLower) {
+    availChars = availChars + lowerChars;
+  }
+
+  var password = []; //Creates password array, needs to be array so push works.
+
+  for(var i = 0; i < passLength; i++) { //For loop for the password length
+    password.push(availChars[Math.floor(Math.random()*availChars.length)]) //Randomly choose an element of the availChars string to be added to the password
+  }
+  return password.join(""); //Join the array into a string so the return is a string
+} 
+
 function writePassword() {
-  if (confirm("Would you like to include special characters?")) {  //Prompts user choice to determine if they would like special characters, uppercase characters, and numbers to be added to the password
-    boolSpecial = true;
-  }
-  if (confirm("Would you like to include uppercase characters?")) {
-    boolUpper = true;
-  }
-  if (confirm("Would you like to include number characters?")) {
-    boolNumbers = true;
-  }
+
+  validateChoices();
 
   var numChars = obtainInput();  //Calls the function to obtain user input on password length
 
-  var password = generatePassword(numChars, boolSpecial, boolUpper, boolNumbers);
+  var password = generatePassword(numChars, boolSpecial, boolUpper, boolNumbers, boolLower);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
