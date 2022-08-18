6b603df232865bf78cd7c7c8acfb90862124009d
cwendland <chris.r.wendland@gmail.com>
2022-08-18 12:30:47 -0700

    Added code comments

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//Booleans to see if the possible password components should be included depending on user choice
 var boolSpecial = false;
 var boolUpper = false;
 var boolNumbers = false;
+
+//Strings of the possible password components depending on user choice
 var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numberChars = "1234567890";
@@ -13,6 +17,7 @@ var lowerChars = upperChars.toLowerCase();
 function generatePassword(passLength, inclSpecial, inclUpper, inclNumbers) {
   var availChars = lowerChars;
 
+  //Conditional statements to determine if optional password components need to be added to the password component string
   if (inclSpecial) {
     availChars = availChars + specialChars;
   }
@@ -23,21 +28,18 @@ function generatePassword(passLength, inclSpecial, inclUpper, inclNumbers) {
     availChars = availChars + numberChars;
   }
 
-  console.log(availChars);
+  var password = []; //Creates password array, needs to be array so push works.
 
-  var password = [];
-
-  for(var i = 0; i < passLength; i++) {
-    password.push(availChars[Math.floor(Math.random()*availChars.length)])
+  for(var i = 0; i < passLength; i++) { //For loop for the password length
+    password.push(availChars[Math.floor(Math.random()*availChars.length)]) //Randomly choose an element of the availChars string to be added to the password
   }
-  console.log(password);
-  return password.join("");
+  return password.join(""); //Join the array into a string so the return is a string
 } 
 
-function obtainInput() {
-  var numChars = prompt("How many characters would you like the password to be?");
+function obtainInput() { //Function to get the password length and make sure input is a number and is between 8 and 128 inclusive
+  var numChars = prompt("How many characters would you like the password to be?"); //Get the desired password length from the user
 
-  if (isNaN(numChars)) {
+  if (isNaN(numChars)) { 
     alert("Invalid submission. Please enter a number.")
     obtainInput();
   } else if (numChars < 8 || numChars > 128){
@@ -50,7 +52,7 @@ function obtainInput() {
 
 
 function writePassword() {
-  if (confirm("Would you like to include special characters?")) {
+  if (confirm("Would you like to include special characters?")) {  //Prompts user choice to determine if they would like special characters, uppercase characters, and numbers to be added to the password
     boolSpecial = true;
   }
   if (confirm("Would you like to include uppercase characters?")) {
@@ -60,7 +62,7 @@ function writePassword() {
     boolNumbers = true;
   }
 
-  var numChars = obtainInput();
+  var numChars = obtainInput();  //Calls the function to obtain user input on password length
 
   var password = generatePassword(numChars, boolSpecial, boolUpper, boolNumbers);
   var passwordText = document.querySelector("#password");
